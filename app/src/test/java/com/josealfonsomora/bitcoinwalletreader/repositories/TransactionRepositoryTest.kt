b5761c15ff1c37351cb6399dd57dc559284c9dae
josealfonsomora <josealfonsomora@gmail.com>
2018-09-09 11:47:43 +0200

    Add JUnit 5 Jupiter

---
+++ app/src/test/java/com/josealfonsomora/bitcoinwalletreader/repositories/TransactionRepositoryTest.kt
@@ -0,0 +1,10 @@
+package com.josealfonsomora.bitcoinwalletreader.repositories
+
+import org.junit.jupiter.api.Test
+
+class TransactionRepositoryTest {
+
+    @Test
+    fun getTransaction() {
+    }
+}
---
+++ app/src/test/java/com/josealfonsomora/bitcoinwalletreader/repositories/WalletRepositoryTest.kt
@@ -0,0 +1,10 @@
+package com.josealfonsomora.bitcoinwalletreader.repositories
+
+import org.junit.jupiter.api.Test
+
+internal class WalletRepositoryTest {
+
+    @Test
+    fun getWallet() {
+    }
+}
