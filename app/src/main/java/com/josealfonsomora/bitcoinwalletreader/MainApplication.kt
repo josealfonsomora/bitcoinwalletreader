6d02e282cffa2ec155eb66c1c6e8e00ca59bd2c8
josealfonsomora <josealfonsomora@gmail.com>
2018-09-10 07:54:41 +0200

    Add JodaTime library
    https://github.com/dlew/joda-time-android

--- app/src/main/java/com/josealfonsomora/bitcoinwalletreader/MainApplication.kt
+++ app/src/main/java/com/josealfonsomora/bitcoinwalletreader/MainApplication.kt
@@ -3,8 +3,13 @@ package com.josealfonsomora.bitcoinwalletreader
 import com.josealfonsomora.bitcoinwalletreader.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import net.danlew.android.joda.JodaTimeAndroid
 
 class MainApplication : DaggerApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        JodaTimeAndroid.init(this);
+    }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().create(this)
